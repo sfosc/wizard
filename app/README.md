968542c14e2ef2b5d6614ca8c321749cab821f2b
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-06-21 19:09:10 +0200

    feat(init) Fully functional playground - testing - automatic favicon generation - markdown support - stepper - mobile-first
    Signed-off-by: Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>

---
+++ app/README.md
@@ -0,0 +1,28 @@
+# SFOSC Wizard
+
+This is a quasar-cli managed Vue.js application.
+
+## Development Setup
+Requirements:
+- node 10LTS
+- npm
+- yarn
+
+```
+$ npm install -g @quasar/cli
+$ git clone %_YOUR-FORK_%
+$ cd app
+$ yarn
+$ quasar dev
+$ quasar build
+```
+
+Please visit https://quasar.dev for information about the Framework.
+
+This is where you can find out about how to use markdown:
+- https://quasarframework.github.io/app-extension-qmarkdown/docs
+
+## LICENSE
+Copyright (c) 2019 the SFOSC Contributors
+
+[MIT ](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/src/layouts/MyLayout.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          SFOSC Wizard
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://sfosc.org/">
+          <q-item-section avatar>
+            <q-icon name="book" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>The Book</q-item-label>
+            <q-item-label caption>sfosc.org</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/sfosc/">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github</q-item-label>
+            <q-item-label caption>github.com/sfosc</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://discord.gg/63Nzb3X">
+          <q-item-section avatar>
+            <q-icon name="record_voice_over" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Discord Chat</q-item-label>
+            <q-item-label caption>chat.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <q-icon class="fixed-center" name="fas fa-globe" style="font-size:60vh;opacity:0.3"></q-icon>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
